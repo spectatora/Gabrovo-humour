57ce54a1c2202734c78e9b414ec7d79fee0bc92e
Iliya Krastinkov <iliya.krastinkov@lecturio.de>
2020-11-15 17:17:56 +0200

    realistic api mock, request states handling

--- js/src/App.js
+++ js/src/App.js
@@ -6,14 +6,75 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
 
+import Grid from '@material-ui/core/Grid';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+import Button from '@material-ui/core/Button';
+import RefreshIcon from '@material-ui/icons/Refresh';
+import Box from '@material-ui/core/Box';
+
 import * as api from './data/api';
 
 function App() {
   const [jokes, setJokes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    api.getJokes().then((response) => setJokes(response));
+    api
+      .getJokes()
+      .then((response) => setJokes(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
   }, []);
+
+  // render loading state
+  if (loading) {
+    return (
+      <Grid
+        container
+        justify="center"
+        alignItems="center"
+        style={{ minHeight: 'calc(100vh - 48px)' }}
+      >
+        <Grid item>
+          <CircularProgress />
+        </Grid>
+      </Grid>
+    );
+  }
+
+  // render error state
+  if (error) {
+    return (
+      <Box p={2}>
+        <Grid
+          container
+          spacing={2}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: 'calc(100vh - 48px)' }}
+        >
+          <Grid item>
+            <ErrorOutlineIcon fontSize="large" />
+          </Grid>
+          <Grid item>
+            <Typography variant="h5">Ops, something went wrong.</Typography>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="outlined"
+              startIcon={<RefreshIcon />}
+              onClick={() => window.location.reload()}
+            >
+              Reload
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+    );
+  }
 
   return (
     <Container>
@@ -33,7 +94,6 @@ function App() {
                 </Typography>
               ))}
               secondaryTypographyProps={{ component: 'div' }}
-              // secondaryTypographyProps={{ noWrap: true }}
             />
           </ListItem>
         ))}
--- js/src/data/api.js
+++ js/src/data/api.js
@@ -1,17 +1,28 @@
-// Use the Fetch API & ES6
-// fetch('data/sources.json')
-// .then(response => response.json())
-// .then(data => {
-//     // Do something with your data
-//     console.log(data);
-// });
-
 import jokes from './jokes.json';
 
+const RESPONSE_DELAY = 2000; // 2 sec
+const ERROR_RATE = 0.3; // 30%
+// const ERROR_RATE = 1;
+
+// mock API communication
+async function apiFetch(success) {
+  // error rate
+  if (Math.random() <= ERROR_RATE) {
+    throw new Error('API error occur.');
+  }
+
+  // response delay
+  return new Promise((resolve) =>
+    setTimeout(() => resolve(success()), RESPONSE_DELAY),
+  );
+}
+
 // TODO: fetch jokes through API
-export async function getJokes() {
-  return jokes.slice(0, 10).map(({ title, joke }) => ({
-    title,
-    content: joke.split('\n'),
-  }));
+export function getJokes() {
+  return apiFetch(() =>
+    jokes.slice(0, 10).map(({ title, joke }) => ({
+      title,
+      content: joke.split('\n'),
+    })),
+  );
 }
