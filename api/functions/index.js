6ea23c46b679f4f6c892433a2e6723adb3bbefc6
lyubomirfilipov <lyubomir.filipov@ffwagency.com>
2020-11-17 01:44:07 +0200

    API Basic methods added. Updated data

--- api/functions/index.js
+++ api/functions/index.js
@@ -12,48 +12,63 @@ admin.initializeApp({
 const db = admin.firestore();
 const app = express();
 app.use(cors({ origin: true }));
+app.use(express.json()) // for parsing application/json
 
-app.get('/hello-world', (req, res) => {
-    return res.status(200).send('Hello World!');
-});
-
-// create
-app.post('/api/create', (req, res) => {
+// create joke
+app.post('/api/jokes', (req, res) => {
     (async () => {
         try {
-
-            console.log(req.body);
-            await db.collection('items').doc('/' + req.body.id + '/')
-                .create({item: req.body.item});
+            await db.collection('jokes').get().then(snapshot => {
+                let currentCount = snapshot.size + 1;
+                let data = req.body;
+                data.created = Date.now();
+                db.collection('jokes').doc('/' + 'ninja' + '/')
+                    .create(req.body);
+            });
             return res.status(200).send();
         } catch (error) {
+            console.log(req.body);
             console.log(error);
             return res.status(500).send(error);
         }
     })();
 });
 
-// read item
-app.get('/api/read/:item_id', (req, res) => {
-    (async () => {
-        try {
-            const document = db.collection('items').doc(req.params.item_id);
-            let item = await document.get();
-            let response = item.data();
-            return res.status(200).send(response);
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
-    })();
-});
 
 // read all
-app.get('/api/read', (req, res) => {
+app.get('/api/jokes', (req, res) => {
     (async () => {
         try {
-            let query = db.collection('items');
-            let response = [];
+            let itemsPerPage = 9;
+            let currentPage = 1;
+            let orderBy = "created";
+            let orderByDirection = "asc";
+            let response = {};
+            let totalItems = 0;
+            let query;
+            await db.collection('jokes').get().then(snapshot => {
+                totalItems = snapshot.size;
+                response.totalItems = snapshot.size;
+                response.totalPages = Math.round(response.totalItems / itemsPerPage);
+                response.currentPage = currentPage;
+            });
+
+            if (currentPage !== 1) {
+                let currentItem = (currentPage - 1) * itemsPerPage + 1;
+                const docRef = db.collection('jokes').doc('/' + currentItem);
+                const snapshot = await docRef.get();
+                query = db.collection('jokes').orderBy(orderBy, orderByDirection).startAt(snapshot).limit(itemsPerPage);
+            } else if (orderBy !== "created") {
+                // we need to apply startAt
+                let currentItem = Math.round(Math.random() * (totalItems - itemsPerPage));
+                const docRef = db.collection('jokes').doc('/' + currentItem);
+                const snapshot = await docRef.get();
+                query = db.collection('jokes').orderByKey().startAt(snapshot).limit(itemsPerPage);
+            } else {
+                query = db.collection('jokes').orderBy(orderBy, orderByDirection).limit(itemsPerPage);
+            }
+
+            response.items = [];
             await query.get().then(querySnapshot => {
                 let docs = querySnapshot.docs;
                 for (let doc of docs) {
@@ -61,7 +76,7 @@ app.get('/api/read', (req, res) => {
                         id: doc.id,
                         item: doc.data().item
                     };
-                    response.push(selectedItem);
+                    response.items.push(selectedItem);
                 }
             });
             return res.status(200).send(response);
@@ -71,35 +86,4 @@ app.get('/api/read', (req, res) => {
         }
     })();
 });
-
-// update
-app.put('/api/update/:item_id', (req, res) => {
-    (async () => {
-        try {
-            const document = db.collection('items').doc(req.params.item_id);
-            await document.update({
-                item: req.body.item
-            });
-            return res.status(200).send();
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
-    })();
-});
-
-// delete
-app.delete('/api/delete/:item_id', (req, res) => {
-    (async () => {
-        try {
-            const document = db.collection('items').doc(req.params.item_id);
-            await document.delete();
-            return res.status(200).send();
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
-    })();
-});
-
 exports.app = functions.https.onRequest(app);
